8aaa21286230cffad7fa4c657a5f37334d10673e
ccfine <469781947@qq.com>
2018-11-26 17:52:03 +0800

    Component -> PureComponent

--- src/Numeric.jsx
+++ src/Numeric.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react"
+import React, { PureComponent } from "react"
 import ReactNumeric from "react-numeric"
 import PropTypes from "prop-types"
 
-export default class Numeric extends Component {
+export default class Numeric extends PureComponent {
   render () {
     return (
       <ReactNumeric style={{ textAlign: "right" }} { ...this.props } />
